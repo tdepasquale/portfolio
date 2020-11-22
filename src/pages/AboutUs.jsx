5e47b8fb548fc0ff11e2d4c1e61e2d0fb56cf2c1
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2020-11-22 01:23:26 -0500

    Services Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { AboutSection } from "../components/AboutSection";
+import { ServicesSection } from "../components/ServicesSection";
 
 export const AboutUs = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection />
     </>
   );
 };
