18c25a75590768e36f5b65f96510a871fab5ddba
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2020-11-22 01:27:56 -0500

    FAQ Section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, iure.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, iure.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, iure.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do You Offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, iure.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AboutSection } from "../components/AboutSection";
+import { FaqSection } from "../components/FaqSection";
 import { ServicesSection } from "../components/ServicesSection";
 
 export const AboutUs = () => {
@@ -7,6 +8,7 @@ export const AboutUs = () => {
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
