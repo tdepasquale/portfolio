b5609afef388feadd5aac4d9cb34bd0f97e3536f
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2020-11-26 00:59:42 -0500

    Added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { OurWork } from "./pages/OurWork";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { MovieDetail } from "./pages/MovieDetail";
 import { AnimatePresence } from "framer-motion";
+import { ScrollTop } from "./components/ScrollTop";
 
 function App() {
   const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
+      <ScrollTop />
       <Nav />
       {/* Framer Motion requires AnimatePresence to wrap the Switch and for Switch to have the location and key as described below for page exit transitions to work */}
       <AnimatePresence exitBeforeEnter>
